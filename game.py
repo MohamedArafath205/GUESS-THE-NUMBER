129736c9bcdb30c292a1c03039f1f4e04af9a313
Mohamed Arafath <mohamedarafath205@gmail.com>
2022-11-26 19:39:50 +0530

    Update some game logic

--- game.py
+++ game.py
@@ -5,7 +5,6 @@ life = 5
 def game():
     global life
     while(life!=0):
-        print(number)
         guess = int(input(Fore.WHITE + "Enter a number between 1 to 30...\n"))
         if (guess==number):
             print(f"{Fore.RED}Congratulations {Fore.YELLOW}you {Fore.GREEN}have {Fore.CYAN}won {Fore.BLUE}this {Fore.MAGENTA}game!{Fore.WHITE}")
@@ -26,3 +25,4 @@ def game():
             break
 
 game()
+input()
