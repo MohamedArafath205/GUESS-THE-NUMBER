e3448257a3765ee1abc3775931d4997d96ceed61
Mohamed Arafath <mohamedarafath205@gmail.com>
2022-11-26 19:34:07 +0530

    Added color to the output text

--- game.py
+++ game.py
@@ -1,26 +1,28 @@
 import random
+from colorama import Fore
 number = random.randint(1,30)
 life = 5
 def game():
     global life
     while(life!=0):
-        guess = int(input("Enter a number between 1 to 30...\n"))
+        print(number)
+        guess = int(input(Fore.WHITE + "Enter a number between 1 to 30...\n"))
         if (guess==number):
-            print("Congratulations you have won this game!")
-            print("Remaining life is : ", life)
-            print("The correct number is : ", number)
+            print(f"{Fore.RED}Congratulations {Fore.YELLOW}you {Fore.GREEN}have {Fore.CYAN}won {Fore.BLUE}this {Fore.MAGENTA}game!{Fore.WHITE}")
+            print(Fore.WHITE + f"Remaining life is : {Fore.GREEN}", life)
+            print(Fore.WHITE + f"The number is : {Fore.GREEN}", number)
             break
         elif (guess<number):
             life -= 1
-            print("The number you have guessed is too low!, higher it a bit")
-            print("Remaining life is : ", life)
+            print(f"{Fore.WHITE}The number you have guessed is too low!, higher it a bit")
+            print(f"Remaining life is : {Fore.RED}", life)
         elif (guess>number):
             life -= 1
-            print("The number you have guessed is too high! lower it a bit")
-            print("Remaining life is : ", life)
+            print(Fore.WHITE + "The number you have guessed is too high! lower it a bit")
+            print(f"Remaining life is : {Fore.RED}", life)
         
         if (life==0):
-            print("The number is : ", number)
+            print(Fore.WHITE + f"The number is : {Fore.RED}", number)
             break
 
 game()
